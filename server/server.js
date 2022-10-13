c34cf64ef99a8ffac353b4d7c58bb5735bcb4ac9
Yair Roshal <Yair7705778@gmail.com>
2022-10-14 00:13:14 +0300

    update server.js

--- server/server.js
+++ server/server.js
@@ -1,16 +1,16 @@
 var express = require('express')
 var cors = require('cors')
-//извлекает всю часть тела входящего потока запросов и предоставляет его для req.body.
-var bodyParser = require('body-parser')  
+//extracts the whole part of the body of the incoming request stream and provides it for req.body.
+var bodyParser = require('body-parser')
 var app = express()
 var mongoose = require('mongoose')
 var port = process.env.PORT || 5500
-// bodyParser.json() : анализирует текст как JSON и выставляет результирующий объект в req.body .
+// bodyParser.json() : parses text as JSON and exposes the resulting object in req.body .
 app.use(bodyParser.json())
-//CORS нужен для того, чтобы разрешить КЛИЕНТУ (браузеру) принять некоторые виды ресурсов.Традиционно браузер может подключить картинки, iframe, скрипты и флеш с других доменов, но не может использовать ajax или web fonts, если на сервере этот домен не внесен в список Access-Control-Allow-Origin.
+//CORS is needed to allow the CUSTOMER (browser) to accept some types of resources.Traditionally, the browser can connect pictures, iframes, scripts and flash from other domains, but cannot use ajax or web fonts if this domain is not listed on the server Access-Control-Allow-Origin.
 app.use(cors())
-// bodyParser.urlencoded() : анализирует текст как кодированные URL-адреса (так как браузеры, как правило, отправляют данные формы из обычных форм, установленных в POST) и выдает результирующий объект (содержащий ключи и значения) в req.body
 app.use(
+	// bodyParser.urlencoded() : parses text as encoded URLs (since browsers tend to send form data from normal forms set to POST) and outputs the resulting object (containing keys and values) to req.body
 	bodyParser.urlencoded({
 		//string or array (when extended is false), or any type (when extended is true).
 		extended: false,
@@ -20,7 +20,7 @@ app.use(
 const mongoURL = 'mongodb://localhost:27017/mernloginreg'
 
 mongoose
-	//useNewUrlParser: true - оно указывает инфраструктуре mongodb, что надо использовать новый парсел адреса сервера.
+	//useNewUrlParser: true - it tells mongodb infrastructure to use the new parsel address of the server.
 	.connect(mongoURL, { useNewUrlParser: true })
 	.then(() => console.log('MongoDb connected'))
 	.catch(err => console.log(err))
